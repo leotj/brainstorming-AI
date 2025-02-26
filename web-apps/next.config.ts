2225c1eb2c0ee8b377575890069a82f7a7201f02
leotj <hi@leotj.com>
2025-02-26 08:28:07 +0700

    chore(build): reduce the docker image size for backend-service and web-apps builds

--- web-apps/next.config.ts
+++ web-apps/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 require('dotenv').config({ path: '../.env' });
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   env: {
     WEB_APPS_SOCKET_HOST: process.env.WEB_APPS_SOCKET_HOST,
     BACKEND_SERVICE_HOST: process.env.BACKEND_SERVICE_HOST
